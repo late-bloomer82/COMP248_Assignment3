056aa0f3a82eb40001cc76bbe0ff54dc55d7c839
late-bloomer82 <nateriver72@outlook.com>
2025-03-21 16:14:13 -0400

    initial commit

---
+++ src/A3_Q2.java
@@ -0,0 +1,9 @@
+/* --------------------------------------------------------------------
+ Assignment 2
+ Written by: Ahmad-Radjai Cherifi, 40327926
+ For COMP 248 Section U – Winter 2025
+ --------------------------------------------------------------------
+ The Quebec Housing Rental Price Index Program calculates the rental price index base don the type and location of a property. The program prompts the user to enter the housing type and location. It then uses ratios to compute the rental price index using the formula. The final index value is displayed to the user.*/
+public class A3_Q2 {
+    public static void main(String[] args) {}
+}
