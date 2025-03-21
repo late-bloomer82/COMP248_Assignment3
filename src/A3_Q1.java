0eef99c6f2d021457975d35568546163be812dc3
late-bloomer82 <nateriver72@outlook.com>
2025-03-21 17:03:26 -0400

    add course processing loops to calculate and display cryptoskill value for each category

--- src/A3_Q1.java
+++ src/A3_Q1.java
@@ -1,5 +1,5 @@
 /* --------------------------------------------------------------------
-Assignment 2
+Assignment 3
 Written by: Ahmad-Radjai Cherifi, 40327926
 For COMP 248 Section U – Winter 2025
 --------------------------------------------------------------------
@@ -55,6 +55,46 @@ public class A3_Q1 {
 
         int cumulativeCryptoSkill = 0;
 
+        // Process each course/category
+        for (int i = 0; i < coursesData.length; i++) {
+            int category = coursesData[i][0];
+            int courseNumber = coursesData[i][1];
+            int meritPoints = coursesData[i][2];
 
+            System.out.print("In the merit points of ");
+
+            // Special handling for ENCS 282
+            if (courseNumber == 282) {
+                System.out.print("ENCS ");
+            } else {
+                System.out.print("COMP ");
+            }
+
+            System.out.println(courseNumber + ", the numbers that are multiple of category " + category + " are: ");
+
+            int courseSum = 0;
+
+            // Process the numbers for this course/category
+            for (int j = category; j <= meritPoints; j += category) {
+                System.out.print(j);
+                courseSum += j;
+
+                // Add comma in between numbers
+                System.out.print(", ");
+            }
+
+            cumulativeCryptoSkill += courseSum;
+
+            System.out.println(" ==> The CryptoSkill value " + cumulativeCryptoSkill + ".");
+
+            // Formatting
+            System.out.println();
+        }
+
+        // Display final message
+        System.out.println("Thank you for using my bespoke Concordia mid-degree Meritas Program!");
+
+        // Close scanner
+        scanner.close();
     }
 }
